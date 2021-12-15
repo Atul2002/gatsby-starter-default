07af3cb639dfc97effa09d5a623b434001d90ef9
Atul2002 <87806810+Atul2002@users.noreply.github.com>
2021-12-16 05:09:37 +0530

    few changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello Everyone</h1>
+    <p>Welcome to my site.</p>
+    <p>Trying to build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
